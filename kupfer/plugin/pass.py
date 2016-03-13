50f33b4eaabb81fbbda93632e6f6cb1c26b80f7c
Michael Klich <michal@michalklich.com>
2016-03-13 19:00:30 +0000

    Passwords get copied to the clipboard

--- kupfer/plugin/pass.py
+++ kupfer/plugin/pass.py
@@ -1,14 +1,16 @@
 __kupfer_name__ = _('Pass')
 __kupfer_sources__ = ('PassSource', )
-__kupfer_actions__ = ('GetPassword', )
+__kupfer_actions__ = ('CopyPassword', )
 __description__ = _('Access to pass')
 __version__ = '0.0.1'
 __author__ = ('Michal Klich <michal@michalklich.com>')
 
 import os
-from subprocess import check_output
+from subprocess import check_call
 
 from kupfer.objects import Action, TextLeaf, Source
+
+PSROOT_PATH = os.path.expanduser('~/.password-store/')
 
 
 class PassSource(Source):
@@ -16,27 +18,29 @@ class PassSource(Source):
         super(PassSource, self).__init__(name)
 
     def get_items(self):
-        # TODO: this needs to return exact ID of the password, with the directory
-        for root, _dirn, filn in os.walk(
-                os.path.expanduser('~/.password-store/')):
+        for root, _dirn, filn in os.walk(PSROOT_PATH):
             for f in filn:
                 name, ext = os.path.splitext(f)
                 if ext == '.gpg':
-                    yield PassLeaf(os.path.join(root, name), name)
+                    abs_path = os.path.join(root, name)
+                    p = os.path.relpath(abs_path, PSROOT_PATH)
+                    yield PassLeaf(p)
 
     def provides(self):
         yield PassLeaf
 
 
-class GetPassword(Action):
+class CopyPassword(Action):
+    def get_description(self):
+        return _('Copy password to the clipboard')
+
     def item_types(self):
         yield PassLeaf
 
     def activate(self, obj):
-        call = check_output(['pass', 'show', obj.name])
-        print(call)
+        check_call(['pass', 'show', '-c', obj.name])
 
 
 class PassLeaf(TextLeaf):
     def get_actions(self):
-        return [GetPassword()]
+        return [CopyPassword()]
